ced9e8918ccec045cf807fbac8121ae6dc5e5532
Antonio González <antoniogonzalezvaldepenas@gmail.com>
2025-12-26 10:28:49 +0100

    First commit

---
+++ AGXFlights/AGXFlights/AGXFlightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AGXFlightsApp.swift
+//  AGXFlights
+//
+//  Created by Antonio González Valdepeñas on 26/12/25.
+//
+
+import SwiftUI
+
+@main
+struct AGXFlightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AGXFlights/AGXFlightsTests/AGXFlightsTests.swift
@@ -0,0 +1,16 @@
+//
+//  AGXFlightsTests.swift
+//  AGXFlightsTests
+//
+//  Created by Antonio González Valdepeñas on 26/12/25.
+//
+
+import Testing
+
+struct AGXFlightsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
