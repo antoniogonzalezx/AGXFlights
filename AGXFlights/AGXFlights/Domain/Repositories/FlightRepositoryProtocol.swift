f491a823ede76b8f2d7a71c08ecc85852ff97752
Antonio González <antoniogonzalezvaldepenas@gmail.com>
2025-12-27 16:48:39 +0100

    Create flight repository protocol

---
+++ AGXFlights/AGXFlights/Domain/Repositories/FlightRepositoryProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  FlightRepositoryProtocol.swift
+//  AGXFlights
+//
+//  Created by Antonio González Valdepeñas on 27/12/25.
+//
+
+import Foundation
+
+protocol FlightRepositoryProtocol: Sendable {
+    func searchFlights(query: String) async throws -> [Flight]
+}
