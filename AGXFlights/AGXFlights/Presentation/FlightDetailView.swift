9a87f39946b7162a72e83a8d153c173450b231a4
Antonio González <antoniogonzalezvaldepenas@gmail.com>
2025-12-28 01:35:07 +0100

    Edit flight row view

--- AGXFlights/AGXFlights/Presentation/FlightDetailView.swift
+++ AGXFlights/AGXFlights/Presentation/FlightDetailView.swift
@@ -12,9 +12,51 @@ struct FlightDetailView: View {
     
     var body: some View {
         List {
-            // TODO: Create detail view
+            Section("Details") {
+                DetailRowView(icon: .globe, title: "Country", value: flight.originCountry)
+                
+                if let latitude = flight.latitude {
+                    DetailRowView(icon: .location, title: "Latitude", value: String(format: "%.4f°", latitude))
+                }
+                
+                if let longitude = flight.longitude {
+                    DetailRowView(icon: .location, title: "Longitude", value: String(format: "%.4f°", longitude))
+                }
+                
+                if let altitude = flight.altitude {
+                    DetailRowView(icon: .altitude, title: "Altitude", value: String(format: "%.0f m", altitude))
+                }
+                
+                if let velocity = flight.velocity {
+                    DetailRowView(icon: .speed, title: "Velocity", value: String(format: "%.0f m/s", velocity))
+                }
+                
+                if let time = flight.timePosition {
+                    DetailRowView(icon: .clock, title: "Last Update", value: time.formatted(date: .abbreviated, time: .shortened))
+                }
+            }
         }
         .navigationTitle(flight.callsign ?? "Flight Details")
+    }
+}
+
+struct DetailRowView: View {
+    let icon: Icons
+    let title: String
+    let value: String
+    
+    var body: some View {
+        HStack(spacing: 8) {
+            icon.image
+                .foregroundStyle(.secondary)
+                .frame(width: 24)
+            
+            Text(title)
+                .foregroundStyle(.secondary)
+                .font(.subheadline)
+            
+            Text(value)
+        }
     }
 }
 
